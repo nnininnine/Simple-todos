9108d5bb5213f497bc560b50a2f442a299bca139
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-06-23 17:12:49 +0700

    feat: implement create todo view controller

--- Simple-todos/Screens/CreateTodo/View/CreateTodoViewController.swift
+++ Simple-todos/Screens/CreateTodo/View/CreateTodoViewController.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import RxCocoa
+import RxSwift
+import UIKit
+
+class CreateTodoViewController: UIViewController {
+    // MARK: Outlets
+
+    @IBOutlet private var textField: UITextField!
+    @IBOutlet private var createButton: UIButton!
+
+    // MARK: Property
+
+    let disposeBag: DisposeBag = .init()
+
+    // MARK: Init
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setup()
+    }
+
+    // MARK: Methods
+
+    func setup() {
+        // todo validation
+        let todoValidator: Observable<Bool> = textField.rx.text.orEmpty.map { $0.count > 0 }
+            .asObservable()
+
+        // binding
+        todoValidator.bind(to: createButton.rx.isEnabled).disposed(by: disposeBag)
+    }
+}
