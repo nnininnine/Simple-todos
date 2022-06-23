fd33eb2b33e71ae90ec648425d377ba5c3d62ca3
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-06-23 13:31:01 +0700

    feat: add create todo screen

---
+++ Simple-todos/Screens/CreateTodo/View/CreateTodoViewController.swift
@@ -0,0 +1,8 @@
+//
+//  CreateTodoViewController.swift
+//  Simple-todos
+//
+//  Created by 7Peaks on 23/6/2565 BE.
+//
+
+import Foundation
---
+++ Simple-todos/Screens/CreateTodo/ViewModel/CreateTodoViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CreateTodoViewModel.swift
+//  Simple-todos
+//
+//  Created by 7Peaks on 23/6/2565 BE.
+//
+
+import Foundation
