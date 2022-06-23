0f68729b5d28eb86238f3b979a2567ecdd9db8c1
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-06-23 12:23:52 +0700

    Initial Commit

---
+++ Simple-todosUITests/Simple_todosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Simple_todosUITestsLaunchTests.swift
+//  Simple-todosUITests
+//
+//  Created by 7Peaks on 23/6/2565 BE.
+//
+
+import XCTest
+
+class Simple_todosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
