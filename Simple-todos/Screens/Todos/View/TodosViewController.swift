e72dee4ec69ab0096849da7ab4f8eb0c246dedcb
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-06-23 14:22:45 +0700

    feat: implement add todo button

--- Simple-todos/Screens/Todos/View/TodosViewController.swift
+++ Simple-todos/Screens/Todos/View/TodosViewController.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import UIKit
+
+class TodosViewController: UIViewController {
+    // MARK: Outlets
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setup()
+    }
+
+    func setup() {
+        // setup add todo button
+        guard let addButton = navigationItem.rightBarButtonItem else { return }
+        addButton.target = self
+        addButton.action = #selector(routeToCreateTodo)
+    }
+
+    @objc func routeToCreateTodo() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "CreateTodoViewController")
+        show(vc, sender: nil)
+    }
+}
