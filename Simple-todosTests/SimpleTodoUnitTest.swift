04e0c29828c8fa805e8e2376bda9c92537e4bc08
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-06-24 15:23:56 +0700

    feat: implement create todo unit test

--- Simple-todosTests/SimpleTodoUnitTest.swift
+++ Simple-todosTests/SimpleTodoUnitTest.swift
@@ -8,9 +8,13 @@
 import Nimble
 import Quick
 import RealmSwift
+@testable import Simple_todos
 import XCTest
 
 class SimpleTodoUnitTest: QuickSpec {
+    var createViewModel: CreateTodoViewModel!
+    var todosViewModel: TodosViewModel!
+    var localRealm: Realm!
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         Realm.Configuration.defaultConfiguration.inMemoryIdentifier = self.name
@@ -21,18 +25,28 @@ class SimpleTodoUnitTest: QuickSpec {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    override func spec() {
+        describe("Create todo") {
+            context("with string") {
+                localRealm = try! Realm(configuration: Realm.Configuration(deleteRealmIfMigrationNeeded: true))
+                let message: String = "test create task"
+                self.createViewModel = .init()
+                self.createViewModel.createTodo(vc: nil, message: message)
+                it("should contain this todo in context") {
+                    let isContain = Array(self.localRealm.objects(Todo.self)).contains(where: { $0.message == message })
+                    expect(isContain).toEventually(equal(true))
+                }
+            }
+            context("with empty string") {
+                localRealm = try! Realm(configuration: Realm.Configuration(deleteRealmIfMigrationNeeded: true))
+                let message: String = ""
+                self.createViewModel = .init()
+                self.createViewModel.createTodo(vc: nil, message: message)
+                it("should contain this todo in context") {
+                    let isContain = Array(self.localRealm.objects(Todo.self)).contains(where: { $0.message == message })
+                    expect(isContain).toEventually(equal(false))
+                }
+            }
         }
     }
 }
