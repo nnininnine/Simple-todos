36f8df3c2f509944f1d778fd35d4f594a6df97b1
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-22 13:16:05 +0700

    feat: add widget extension target

---
+++ TodosWidget/View/TodoWidgetView.swift
@@ -0,0 +1,20 @@
+//
+//  TodoWidgetView.swift
+//  Simple-todos
+//
+//  Created by 7Peaks on 22/7/2565 BE.
+//
+
+import SwiftUI
+
+struct TodoWidgetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TodoWidgetView_Previews: PreviewProvider {
+    static var previews: some View {
+        TodoWidgetView()
+    }
+}
---
+++ TodosWidget/Widget/TodosWidget.swift
@@ -0,0 +1,67 @@
+//
+//  TodosWidget.swift
+//  TodosWidget
+//
+//  Created by 7Peaks on 22/7/2565 BE.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date())
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date())
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate)
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+}
+
+struct TodosWidgetEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        Text(entry.date, style: .time)
+    }
+}
+
+@main
+struct TodosWidget: Widget {
+    let kind: String = "TodosWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            TodosWidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+struct TodosWidget_Previews: PreviewProvider {
+    static var previews: some View {
+        TodosWidgetEntryView(entry: SimpleEntry(date: Date()))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
