f4556aab7e1c3bcb3026d0038291b19503975ae9
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-26 13:45:14 +0700

    feat(intent): implement IntentHandler

--- IntentSelectTask/IntentHandler.swift
+++ IntentSelectTask/IntentHandler.swift
@@ -8,12 +8,20 @@
 import Intents
 
 class IntentHandler: INExtension {
-    
-    override func handler(for intent: INIntent) -> Any {
-        // This is the default implementation.  If you want different objects to handle different intents,
-        // you can override this and return the handler you want for that particular intent.
-        
-        return self
-    }
-    
+  override func handler(for intent: INIntent) -> Any {
+    // This is the default implementation.  If you want different objects to handle different intents,
+    // you can override this and return the handler you want for that particular intent.
+
+    return self
+  }
+}
+
+// MARK: - SelectTaskIntentHandling
+
+extension IntentHandler: SelectTaskIntentHandling {
+  func provideTaskOptionsCollection(for intent: SelectTaskIntent, with completion: @escaping (INObjectCollection<TaskINO>?, Error?) -> Void) {
+    let tasks: [TaskINO] = TodosService.shared.getAllTodos().map { TaskINO(identifier: $0.id?.uuidString, display: "\($0.message)") }
+
+    completion(INObjectCollection(items: tasks), nil)
+  }
 }
